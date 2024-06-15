9c9e2f0ac0181b67b47dd4c8ad6e4c08f114f25a
nrn <nrninsane@gmail.com>
2024-06-15 10:27:41 +0200

    consider the token stream as context-free

---
+++ token_stream.zig
@@ -0,0 +1,148 @@
+const std = @import("std");
+
+pub const TokenStream = struct {
+    text: []const u8,
+    idx: usize = 0,
+
+    const Self = @This();
+
+    fn next(self: *Self) ?Token {
+        if (self.idx >= self.text.len) return null;
+
+        const tokenTag = self.currentCharType();
+        const start = self.idx;
+        while (self.idx < self.text.len and self.currentCharType() == tokenTag) : (self.idx += 1) {}
+        return Token.create(tokenTag, self.text[start..self.idx]);
+    }
+
+    fn currentCharType(self: Self) TokenTag {
+        return switch (self.text[self.idx]) {
+            'a'...'z', 'A'...'Z' => .alphanumerical,
+            ' ', '\t', '\n', '\r' => .space,
+            else => .symbol,
+        };
+    }
+};
+
+const Token = union(TokenTag) {
+    alphanumerical: []const u8,
+    space: []const u8,
+    symbol: []const u8,
+
+    pub fn create(tag: TokenTag, content: []const u8) Token {
+        return switch (tag) {
+            .alphanumerical => Token{ .alphanumerical = content },
+            .space => Token{ .space = content },
+            .symbol => Token{ .symbol = content },
+        };
+    }
+
+    fn debugInfo(self: Token) []const u8 {
+        return switch (self) {
+            .alphanumerical => |c| c,
+            .space => |c| c,
+            .symbol => |c| c,
+        };
+    }
+};
+
+const TokenTag = enum { alphanumerical, space, symbol };
+
+test "TokenStream" {
+    const text =
+        \\hello moto
+        \\# moto moto
+        \\moo desu desu **`kawaiii`**_
+        \\
+        \\issa new paragraph
+        \\gotta tell you about shell-scripts man
+        \\```sh
+        \\echo hello world;
+        \\```
+    ;
+
+    var token_list = std.ArrayList(Token).init(std.testing.allocator);
+    defer token_list.deinit();
+    var token_stream = TokenStream{ .text = text };
+    while (token_stream.next()) |token| {
+        try token_list.append(token);
+    }
+
+    try expectEqualTokens(
+        token_list,
+        &[_]Token{
+            Token{ .alphanumerical = "hello" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "moto" },
+            Token{ .space = "\n" },
+            Token{ .symbol = "#" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "moto" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "moto" },
+            Token{ .space = "\n" },
+            Token{ .alphanumerical = "moo" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "desu" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "desu" },
+            Token{ .space = " " },
+            Token{ .symbol = "**`" },
+            Token{ .alphanumerical = "kawaiii" },
+            Token{ .symbol = "`**_" },
+            Token{ .space = "\n\n" },
+            Token{ .alphanumerical = "issa" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "new" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "paragraph" },
+            Token{ .space = "\n" },
+            Token{ .alphanumerical = "gotta" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "tell" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "you" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "about" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "shell" },
+            Token{ .symbol = "-" },
+            Token{ .alphanumerical = "scripts" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "man" },
+            Token{ .space = "\n" },
+            Token{ .symbol = "```" },
+            Token{ .alphanumerical = "sh" },
+            Token{ .space = "\n" },
+            Token{ .alphanumerical = "echo" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "hello" },
+            Token{ .space = " " },
+            Token{ .alphanumerical = "world" },
+            Token{ .symbol = ";" },
+            Token{ .space = "\n" },
+            Token{ .symbol = "```" },
+        },
+    );
+}
+
+fn expectEqualTokens(token_list: std.ArrayList(Token), expected_tokens: []const Token) !void {
+    const received_tokens = token_list.items;
+    std.testing.expectEqualDeep(expected_tokens, received_tokens) catch |err| {
+        std.debug.print("—" ** 80 ++ "\n", .{});
+        var i: usize = 0;
+        const len = @max(received_tokens.len, expected_tokens.len);
+        while (i < len) : (i += 1) {
+            const received = if (i < received_tokens.len) received_tokens[i].debugInfo() else "none";
+            const expected = if (i < expected_tokens.len) expected_tokens[i].debugInfo() else "none";
+            if (!std.mem.eql(u8, received, expected)) {
+                std.debug.print("received[{any}]: {s}\n", .{ i, received });
+                std.debug.print("expected[{any}]: {s}\n", .{ i, expected });
+            }
+        }
+        std.debug.print("—" ** 80 ++ "\n", .{});
+        std.debug.print("{any}\n", .{err});
+        std.debug.print("—" ** 80 ++ "\n", .{});
+        return err;
+    };
+}

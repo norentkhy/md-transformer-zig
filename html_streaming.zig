961abbbe5cf42b5b1d702f9a965b6432bb12232d
nrn <nrninsane@gmail.com>
2024-07-15 00:57:31 +0200

    feat no line-breaks if file ends on it

--- html_streaming.zig
+++ html_streaming.zig
@@ -2,158 +2,142 @@ const std = @import("std");
 const print = std.debug.print;
 const assert = std.debug.assert;
 const MdToken = @import("./md_streaming.zig").Token;
+const MdTokenTag = @import("./md_streaming.zig").TokenTag;
 const MdTokenStream = @import("./md_streaming.zig").TokenStream;
+const FixedQueue = @import("./fixed_queue.zig").FixedQueue;
 
-const FromMarkdownStream = struct {
-    token_stream: *MdTokenStream,
-    ancestry: std.ArrayList(ElementType),
-
-    current_parent: ElementType = .body,
-    next_html_token: ?HtmlToken = null,
-    next_unprocessed_token: ?MdToken = null,
-    close_parent: bool = false,
-    close_stream: bool = false,
-
+const HtmlFromMdStream = struct {
     const Self = @This();
+    const HtmlQueue = FixedQueue(HtmlToken, 3);
+    const MdQueue = FixedQueue(MdToken, 3);
 
-    pub fn init(token_stream: *MdTokenStream, allocator: std.mem.Allocator) !Self {
-        var ancestry = std.ArrayList(ElementType).init(allocator);
-        try ancestry.append(.body);
+    md_token_stream: *MdTokenStream,
+    context_stack: std.ArrayList(Context),
+    html_queue: HtmlQueue = HtmlQueue{},
+    md_queue: MdQueue = MdQueue{},
+
+    pub fn init(md_token_stream: *MdTokenStream, allocator: std.mem.Allocator) !Self {
         return .{
-            .token_stream = token_stream,
-            .ancestry = ancestry,
+            .md_token_stream = md_token_stream,
+            .context_stack = std.ArrayList(Context).init(allocator),
         };
     }
 
     pub fn deinit(self: Self) void {
-        self.ancestry.deinit();
+        self.context_stack.deinit();
     }
 
     pub fn next(self: *Self, allocator: std.mem.Allocator) !?HtmlToken {
-        if (self.next_html_token) |token| {
-            self.next_html_token = null;
-            return token;
-        }
+        if (self.html_queue.take()) |html_token| return html_token;
 
-        if (self.close_parent) return self.closeParent();
-        if (self.close_stream) return self.closeStream();
+        if (self.peekMdToken(0)) |md_token| {
+            const context = self.context_stack.getLastOrNull() orelse Context.from(md_token);
+            if (self.context_stack.items.len == 0) {
+                try self.context_stack.append(context);
+                return context.startTag();
+            }
 
-        return switch (self.current_parent) {
-            .body => try self.nextFromBody(),
-            .paragraph => try self.nextFromParagraph(allocator),
-        };
-    }
+            var text_buffer = std.ArrayList(u8).init(allocator);
+            defer text_buffer.deinit();
 
-    fn closeParent(self: *Self) HtmlToken {
-        assert(self.ancestry.items.len > 0);
-        self.close_parent = false;
-        const parent_to_close = self.ancestry.pop();
-        self.current_parent = self.ancestry.getLast();
-        return parent_to_close.closingToken();
-    }
-
-    fn closeStream(self: *Self) ?HtmlToken {
-        self.close_stream = true;
-        if (self.ancestry.items.len == 1) return null;
-
-        const parent_to_close = self.ancestry.pop();
-        self.current_parent = self.ancestry.getLast();
-        return parent_to_close.closingToken();
-    }
-
-    fn nextFromBody(self: *Self) !?HtmlToken {
-        while (self.nextUnprocessedToken()) |token| {
-            switch (token) {
-                .alphanumerical => {
-                    try self.ancestry.append(.paragraph);
-                    self.current_parent = .paragraph;
-                    self.next_unprocessed_token = token;
-                    return HtmlToken.paragraph_opening;
+            switch (context) {
+                .paragraph => {
+                    try self.parseParagraph(&text_buffer);
+                    print("paragraph: {s}\n", .{text_buffer.items});
+                    return HtmlToken{ .text_node = try toNewOwner(allocator, &text_buffer) };
                 },
-                .space => {},
-                .symbol => @panic("dem symbols maine"),
             }
         }
-        return self.closeStream() orelse null;
+
+        return null;
     }
 
-    fn nextFromParagraph(self: *Self, allocator: std.mem.Allocator) !?HtmlToken {
-        var text_buffer = std.ArrayList(u8).init(allocator);
-        defer text_buffer.deinit();
+    fn peekMdToken(self: *Self, idx: usize) ?MdToken {
+        while (self.md_queue.len <= idx) {
+            const md_token = self.md_token_stream.next() orelse return null;
+            self.md_queue.put(md_token);
+        }
+        return self.md_queue.peek(idx);
+    }
 
-        ingestion: while (self.nextUnprocessedToken()) |token| {
-            switch (token) {
+    fn nextMdToken(self: *Self) ?MdToken {
+        return self.md_queue.take() orelse self.md_token_stream.next();
+    }
+
+    fn parseParagraph(self: *Self, text_buffer: *std.ArrayList(u8)) !void {
+        ingestion: while (self.nextMdToken()) |md_token| {
+            switch (md_token) {
                 .space => |content| {
                     var new_line_count: usize = 0;
                     for (content) |char| {
                         if (char == '\n') new_line_count += 1;
                     }
 
-                    switch (new_line_count) {
-                        0 => {
-                            try text_buffer.append(' ');
-                            continue :ingestion;
-                        },
-                        1 => {
-                            self.next_html_token = HtmlToken.line_break;
-                            break :ingestion;
-                        },
-                        else => {
-                            self.close_parent = true;
-                            break :ingestion;
-                        },
+                    if (self.peekMdToken(0) == null or new_line_count > 1) {
+                        const context = self.context_stack.pop();
+                        self.html_queue.put(context.endTag());
+                        break :ingestion;
                     }
+
+                    if (new_line_count == 1) {
+                        self.html_queue.put(HtmlToken.line_break);
+                        break :ingestion;
+                    }
+
+                    try text_buffer.append(' ');
+                    continue :ingestion;
                 },
 
-                .symbol => @panic("dem symbols maine"),
+                .symbol => @panic("dem symbols"),
 
                 .alphanumerical => |content| {
-                    print("content: {s}\n", .{content});
                     for (content) |char| try text_buffer.append(char);
                 },
             }
         }
-        if (text_buffer.items.len > 0) {
-            self.close_parent = true;
-            return HtmlToken{ .text_node = try toNewOwner(allocator, &text_buffer) };
-        }
-
-        print("bye bye\n", .{});
-        return null;
-    }
-
-    fn nextUnprocessedToken(self: *Self) ?MdToken {
-        if (self.next_unprocessed_token) |token| {
-            self.next_unprocessed_token = null;
-            return token;
-        }
-        return self.token_stream.next();
     }
 };
+
+const Context = union(ContextTag) {
+    const Self = @This();
+    paragraph: HtmlTagPair,
+
+    pub fn from(md_token: MdToken) Self {
+        return switch (md_token) {
+            .alphanumerical => Self{ .paragraph = HtmlTagPair{
+                .start_tag = HtmlToken.paragraph_start,
+                .end_tag = HtmlToken.paragraph_end,
+            } },
+            else => unreachable,
+        };
+    }
+
+    pub fn startTag(self: Self) HtmlToken {
+        return switch (self) {
+            inline else => |context| context.start_tag,
+        };
+    }
+
+    pub fn endTag(self: Self) HtmlToken {
+        return switch (self) {
+            inline else => |context| context.end_tag,
+        };
+    }
+};
+
+const ContextTag = enum { paragraph };
 
 fn toNewOwner(allocator: std.mem.Allocator, text_buffer: *std.ArrayList(u8)) !std.ArrayList(u8) {
     const slice = try text_buffer.toOwnedSlice();
     return std.ArrayList(u8).fromOwnedSlice(allocator, slice);
 }
 
-const Tree = struct { type: ElementType };
-
-const ElementType = enum {
-    body,
-    paragraph,
-
-    fn closingToken(self: ElementType) HtmlToken {
-        return switch (self) {
-            .paragraph => HtmlToken.paragraph_closing,
-            .body => @panic("ain't go need for no closing body yet yo"),
-        };
-    }
-};
+const HtmlTagPair = struct { start_tag: HtmlToken, end_tag: HtmlToken };
 
 const HtmlToken = union(HtmlTag) {
     line_break,
-    paragraph_opening,
-    paragraph_closing,
+    paragraph_start,
+    paragraph_end,
     text_node: std.ArrayList(u8),
 
     pub fn deinit(self: HtmlToken) void {
@@ -166,8 +150,8 @@ const HtmlToken = union(HtmlTag) {
 
 const HtmlTag = enum {
     line_break,
-    paragraph_opening,
-    paragraph_closing,
+    paragraph_start,
+    paragraph_end,
     text_node,
 };
 
@@ -227,11 +211,15 @@ test "test two next calls for one paragraph" {
         // MdToken{ .symbol = "```" },
     };
     var md_token_stream = MdTokenStream.fromTokens(&tokens);
-    var parser = try FromMarkdownStream.init(&md_token_stream, std.testing.allocator);
+    var parser = try HtmlFromMdStream.init(&md_token_stream, std.testing.allocator);
     defer parser.deinit();
 
+    var count: u8 = 0;
     while (try parser.next(std.testing.allocator)) |token| {
+        count += 1;
+        if (count > 8) break;
+
         defer token.deinit();
-        print("parsed: {any}\n", .{token});
+        print("parsed: {any}\n\n", .{token});
     }
 }
